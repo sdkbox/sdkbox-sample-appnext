44f215679b28d6bcd355c4aed88bd4fd4f09dae3
yinjimmy <jimmy.yin5@gmail.com>
2016-05-11 10:41:37 +0800

    update Lua sample

--- lua/frameworks/runtime-src/proj.ios_mac/ios/RootViewController.mm
+++ lua/frameworks/runtime-src/proj.ios_mac/ios/RootViewController.mm
@@ -75,11 +75,12 @@
 }
 
 - (BOOL) shouldAutorotate {
-    if (SimpleConfigParser::getInstance()->isLanscape()) {
-        return YES;
-    }else{
-        return NO;
-    }
+    return YES;
+//    if (SimpleConfigParser::getInstance()->isLanscape()) {
+//        return YES;
+//    }else{
+//        return NO;
+//    }
 }
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
